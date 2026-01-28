b5d63b0a52200ab66cf4e9abb10bc37e665015fc
Illia <lpodildaviduk@gmail.com>
2026-01-28 20:58:38 +0100

    added user flow tests

---
+++ tests/test_board_svc/test_board_svc.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+# def test_user_login(client):
+#     client.post(
+#         "/api/v1/auth/sign_up",
+#         json={
+#             "email": "user@example.com",
+#             "password": "SuperPassword!23",
+#         },
+#     )
--- tests/test_user_svc/test_user_svc.py
+++ tests/test_user_svc/test_user_svc.py
@@ -14,6 +14,19 @@ def test_user_creation(client):
     assert data["email"] == "user@example.com"
 
 
+def test_user_creation_fail(client):
+    result = client.post(
+        "/api/v1/auth/sign_up",
+        json={
+            "email": "userexample.com",
+            "password": "SuperPassword!23",
+        },
+    )
+    data = result.json()
+    assert result.status_code == 422
+    assert "detail" in data
+
+
 def test_user_login(client):
     client.post(
         "/api/v1/auth/sign_up",
@@ -30,5 +43,64 @@ def test_user_login(client):
         },
     )
     data = result.json()
-    assert len(data["access_token"]) >= 8
+    assert "access_token" in data
     assert data["token_type"] == "Bearer"
+
+
+def test_user_logout(client):
+    client.post(
+        "/api/v1/auth/sign_up",
+        json={
+            "email": "user@example.com",
+            "password": "SuperPassword!23",
+        },
+    )
+    client.post(
+        "/api/v1/auth/login",
+        json={
+            "email": "user@example.com",
+            "password": "SuperPassword!23",
+        },
+    )
+    result = client.post("/api/v1/auth/logout")
+    data = result.json()
+    assert result.status_code == 200
+    assert "message" in data
+
+
+def test_me(client):
+    client.post(
+        "/api/v1/auth/sign_up",
+        json={
+            "email": "user@example.com",
+            "password": "SuperPassword!23",
+        },
+    )
+    client.post(
+        "/api/v1/auth/login",
+        json={
+            "email": "user@example.com",
+            "password": "SuperPassword!23",
+        },
+    )
+    result = client.get("/api/v1/auth/me")
+    data = result.json()
+    assert result.status_code == 200
+    assert "id" in data
+
+
+def test_me_blank(client):
+    result = client.get("/api/v1/auth/me")
+    assert result.status_code == 401
+
+
+def test_me_without_login(client):
+    client.post(
+        "/api/v1/auth/sign_up",
+        json={
+            "email": "user@example.com",
+            "password": "SuperPassword!23",
+        },
+    )
+    result = client.get("/api/v1/auth/me")
+    assert result.status_code == 401
