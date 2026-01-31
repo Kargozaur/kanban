2f8c41f21e13a51a7cfa23002d73f5e5de3398fa
Illia <lpodildaviduk@gmail.com>
2026-01-31 01:53:37 +0100

    schemas for the member repo

---
+++ backend/schemas/member_schema.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel, Field
+from backend.core.utility.role_enum import RoleEnum
+from typing import Annotated, Optional
+from uuid import UUID
+
+
+class AddBoardMemberBase(BaseModel):
+    board_id: Annotated[
+        int, Field(gt=0, description="Id of the current board")
+    ]
+    role: Annotated[
+        RoleEnum,
+        Field(
+            RoleEnum.VIEWER,
+            examples=[
+                "admin",
+                "viewer",
+                "member",
+            ],
+            description="By default ads user as a viewer. \n"
+            "This behavior may be adjusted with the patch request \n"
+            "by the user with role of the admin",
+        ),
+    ]
+
+
+class AddBoardMemberEmail(AddBoardMemberBase):
+    user_email: Annotated[
+        str,
+        Field(
+            ...,
+            description="Id of the user from the appropriate table",
+        ),
+    ]
+
+
+class AddBoardMemberUUID(AddBoardMemberBase):
+    user_id: Annotated[
+        UUID, Field(..., description="User's id from the database")
+    ]
+
+
+class UpdateBoardMember(BaseModel):
+    role: Annotated[
+        Optional[RoleEnum],
+        Field(
+            default=RoleEnum.VIEWER,
+            description="Changes the role of the user inside the board",
+        ),
+    ]
